5080eb9ef7c6ea7e77a5480401ceb0b1c27661d7
GoAndrii <soki200561@gmail.com>
2022-08-16 21:01:16 +0300

    Finisg

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,22 +1,12 @@
-const BASIC_URL = `https://restcountries.com/v3.1/name`;
-const FILTER_RESPONSE = `fields=name,capital,population,flags,languages`;
+const BASE_URL = 'https://restcountries.com/v3.1';
+const FILTER_RESPONSE = 'fields=name,capital,population,flags,languages';
 
-//Export fetchCountries
-//Will create fetch only if name is not an empty string
-
-export default function fetchCountries(name) {
-  if (name) {
-    return fetch(`${BASIC_URL}/${name}?${FILTER_RESPONSE}`).then(
-      onSuccessFetch
-    );
-  }
-}
-
-//fetch will throw error if 404
-//otherwise fetch will parse response
-function onSuccessFetch(response) {
-  if (!response.ok) {
-    throw new Error(response.status);
-  }
-  return response.json();
+export function fetchCountries(name) {
+  const url = `${BASE_URL}/name/${name}?${FILTER_RESPONSE}`;
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
